712b4095f22d488a9fd8e44b87036e2b15454496
Zeggriim <lilian.dorazio@hotmail.fr>
2023-01-24 10:02:47 +0100

    Clear

--- src/Enum/Region.php
+++ src/Enum/Region.php
@@ -6,14 +6,14 @@ namespace Zeggriim\RiotApiDatadragon\Enum;
 
 class Region extends AbstractEnum
 {
-    public const AMERICAS = "AMERICAS";
-    public const ASIA = "ASIA";
-    public const EUROPE = "EUROPE";
-    public const SEA = "SEA";
+    public const AMERICAS   = "AMERICAS";
+    public const ASIA       = "ASIA";
+    public const EUROPE     = "EUROPE";
+    public const SEA        = "SEA";
 
-    public const HOST = ".api.riotgames.com";
-    public const HOS_AMERICAS = self::HOST . self::HOST;
-    public const HOS_ASIA = self::ASIA . self::HOST;
-    public const HOS_EUROPE = self::EUROPE . self::HOST;
-    public const HOS_SEA = self::SEA . self::HOST;
+    private const HOST          = ".api.riotgames.com";
+    public const HOS_AMERICAS   = self::HOST    . self::HOST;
+    public const HOS_ASIA       = self::ASIA    . self::HOST;
+    public const HOS_EUROPE     = self::EUROPE  . self::HOST;
+    public const HOS_SEA        = self::SEA     . self::HOST;
 }
