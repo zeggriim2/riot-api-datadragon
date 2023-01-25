df1166db62cb079838ca5c9f72fe3b31fb6b5717
Zeggriim <lilian.dorazio@hotmail.fr>
2023-01-25 15:17:22 +0100

    Fix Denormalizer

--- src/Serializer/SerializerFactory.php
+++ src/Serializer/SerializerFactory.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace Zeggriim\RiotApiDatadragon\Serializer;
 
+
 use Symfony\Component\PropertyInfo\Extractor\ReflectionExtractor;
+use Symfony\Component\Serializer\Normalizer\ArrayDenormalizer;
 use Symfony\Component\Serializer\Normalizer\DateTimeNormalizer;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 use Symfony\Component\Serializer\Serializer as SymfonySerializer;
@@ -22,8 +24,16 @@ class SerializerFactory
                 null,
                 new ReflectionExtractor()
             );
-            self::$serializer = new \Symfony\Component\Serializer\Serializer([new DateTimeNormalizer(), $normalizer]);
+
+            self::$serializer = new \Symfony\Component\Serializer\Serializer(
+                [
+                    new ArrayDenormalizer(),
+                    new DateTimeNormalizer(),
+                    $normalizer,
+                ]
+            );
         }
+
         return self::$serializer;
     }
 }
