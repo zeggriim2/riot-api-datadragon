b39bddc68d11e0b4b6795718f2ca70947a2a79e6
Zeggriim <zeggriim@gmail.com>
2024-06-21 23:38:55 +0200

    FEAT : - Add 3 ENUM on TIER

--- src/Enum/Tier.php
+++ src/Enum/Tier.php
@@ -6,11 +6,14 @@ namespace Zeggriim\RiotApiDataDragon\Enum;
 
 enum Tier: string
 {
-    case IRON       = 'IRON';
-    case BRONZE     = 'BRONZE';
-    case SILVER     = 'SILVER';
-    case GOLD       = 'GOLD';
-    case PLATINUM   = 'PLATINUM';
-    case EMERALD    = 'EMERALD';
-    case DIAMOND    = 'DIAMOND';
+    case IRON           = 'IRON';
+    case BRONZE         = 'BRONZE';
+    case SILVER         = 'SILVER';
+    case GOLD           = 'GOLD';
+    case PLATINUM       = 'PLATINUM';
+    case EMERALD        = 'EMERALD';
+    case DIAMOND        = 'DIAMOND';
+    case MASTER         = 'MASTER';
+    case GRANDMASTER    = 'GRANDMASTER';
+    case CHALLENGER     = 'CHALLENGER';
 }
