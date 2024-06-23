d9467ece9156b180a1e7b20ac3c844b84cf5dfab
Zeggriim <zeggriim@gmail.com>
2024-06-23 18:18:37 +0200

    FIX : - rename and value Enum Queue

--- src/Enum/Queue.php
+++ src/Enum/Queue.php
@@ -7,6 +7,6 @@ namespace Zeggriim\RiotApiDataDragon\Enum;
 enum Queue: string
 {
     case RANKED_SOLO = 'RANKED_SOLO_5x5';
-    case RANKED_FLED_SR = 'RANKED_FLED_SR';
-    case RANKED_FLED_TT = 'RANKED_FLED_TT';
+    case RANKED_FLEX_SR = 'RANKED_FLEX_SR';
+    case RANKED_FLEX_TT = 'RANKED_FLEX_TT';
 }
