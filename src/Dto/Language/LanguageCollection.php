57442f04f415eea90cbfb4443f26a13f4a5edf8d
zeggriim <zeggriim@gmail.com>
2025-08-04 23:26:44 +0200

    feat(Dto) : ZEG-43 Add DTO Language return api Language

---
+++ src/Dto/Language/LanguageCollection.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Zeggriim\RiotApiDataDragon\Dto\Language;
+
+final class LanguageCollection implements \Countable, \IteratorAggregate
+{
+    /**
+     * @param Language[] $languages
+     */
+    public function __construct(
+        private array $languages = [],
+    ) {
+    }
+
+    public function addLanguage(Language $language): void
+    {
+        $this->languages[$language->code] = $language;
+    }
+
+    /**
+     * @return Language[]
+     */
+    public function getLanguages(): array
+    {
+        return $this->languages;
+    }
+
+    public function getLanguage(string $code): ?Language
+    {
+        return $this->languages[$code] ?? null;
+    }
+
+    public function count(): int
+    {
+        return \count($this->languages);
+    }
+
+    public function getIterator(): \Traversable
+    {
+        return new \ArrayIterator($this->languages);
+    }
+
+    public function hasLanguage(string $code): bool
+    {
+        return isset($this->languages[$code]);
+    }
+}
---
+++ src/Serializer/Normalizer/LanguageCollectionNormalizer.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Zeggriim\RiotApiDataDragon\Serializer\Normalizer;
+
+use Symfony\Component\Serializer\Normalizer\DenormalizerAwareInterface;
+use Symfony\Component\Serializer\Normalizer\DenormalizerAwareTrait;
+use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
+use Zeggriim\RiotApiDataDragon\Dto\Language\Language;
+use Zeggriim\RiotApiDataDragon\Dto\Language\LanguageCollection;
+
+final class LanguageCollectionNormalizer implements DenormalizerInterface, DenormalizerAwareInterface
+{
+    use DenormalizerAwareTrait;
+
+    /**
+     * @param array<string, string> $data
+     */
+    public function denormalize(mixed $data, string $type, ?string $format = null, array $context = []): LanguageCollection
+    {
+        $collection = new LanguageCollection();
+
+        foreach ($data as $code) {
+            $language = new Language($code);
+            $collection->addLanguage($language);
+        }
+
+        return $collection;
+    }
+
+    public function supportsDenormalization(mixed $data, string $type, ?string $format = null, array $context = []): bool
+    {
+        return LanguageCollection::class === $type && \is_array($data);
+    }
+
+    public function getSupportedTypes(?string $format): array
+    {
+        return [
+            LanguageCollection::class => true,
+        ];
+    }
+}
