2dd8d4a3b571700feb7f1dc17c2376478b26beb6
zeggriim <zeggriim@gmail.com>
2025-08-04 23:26:44 +0200

    feat(Dto) : ZEG-43 Add DTO Language return api Language

--- tests/Endpoint/DataDragon/LanguageApiTest.php
+++ tests/Endpoint/DataDragon/LanguageApiTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace Zeggriim\RiotApiDataDragon\Tests\Endpoint\DataDragon;
 
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Zeggriim\RiotApiDataDragon\Dto\Language\LanguageCollection;
 use Zeggriim\RiotApiDataDragon\Endpoint\DataDragon\LanguageApi;
 use Zeggriim\RiotApiDataDragon\Tests\Traits\RiotApiDataDragonTrait;
 
@@ -30,5 +31,21 @@ class LanguageApiTest extends KernelTestCase
         self::assertSame($data[4], $languages[4]);
         self::assertSame($data[5], $languages[5]);
         self::assertSame($data[6], $languages[6]);
+    }
+
+    public function testGetLanguageCollection(): void
+    {
+        $data = ['en_US', 'cs_CZ', 'de_DE', 'en_GB','en_PH', 'fr_FR', 'zh_MY'];
+
+        $languageApi = $this->getLanguageApi($data);
+        $languagesCollection = $languageApi->getLanguagesAsCollection();
+
+        self::assertInstanceOf(LanguageCollection::class, $languagesCollection);
+        self::assertNotEmpty($languagesCollection->getLanguages());
+        self::assertCount(count($data), $languagesCollection->getLanguages());
+        self::assertSame(count($data), $languagesCollection->count());
+        self::assertTrue($languagesCollection->hasLanguage($data[0]));
+        self::assertSame($data[0], $languagesCollection->getLanguage($data[0])?->code);
+        self::assertNull($languagesCollection->getLanguage('test'));
     }
 }
