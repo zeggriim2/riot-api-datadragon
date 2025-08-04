fb49f4a3e1adcbb40a519d684a56db305d3ec177
zeggriim <zeggriim@gmail.com>
2025-08-04 23:26:44 +0200

    feat(Dto) : ZEG-43 Add DTO Language return api Language

--- src/Endpoint/DataDragon/LanguageApiInterface.php
+++ src/Endpoint/DataDragon/LanguageApiInterface.php
@@ -4,7 +4,11 @@ declare(strict_types=1);
 
 namespace Zeggriim\RiotApiDataDragon\Endpoint\DataDragon;
 
+use Zeggriim\RiotApiDataDragon\Dto\Language\LanguageCollection;
+
 interface LanguageApiInterface
 {
     public function getLanguages(): array;
+
+    public function getLanguagesAsCollection(): LanguageCollection;
 }
---
+++ src/Transformer/LanguageTransformer.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Zeggriim\RiotApiDataDragon\Transformer;
+
+use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
+use Zeggriim\RiotApiDataDragon\Dto\Language\LanguageCollection;
+
+final class LanguageTransformer
+{
+    public function __construct(
+        private readonly DenormalizerInterface $denormalizer,
+    ) {
+    }
+
+    public function transformToCollection(array $data): LanguageCollection
+    {
+        return $this->denormalizer->denormalize($data, LanguageCollection::class);
+    }
+}
