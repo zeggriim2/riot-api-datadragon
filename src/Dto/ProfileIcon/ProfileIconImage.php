5fd8c0278c0eafde32ebcc999914cae6a479b75a
zeggriim <zeggriim@gmail.com>
2025-08-07 00:10:20 +0200

    feat(Dto) : ZEG-44 Add DTO ProfileIcon return api ProfileIcon

---
+++ src/Dto/ProfileIcon/ProfileIconImage.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Zeggriim\RiotApiDataDragon\Dto\ProfileIcon;
+
+final readonly class ProfileIconImage
+{
+    public function __construct(
+        public string $full,
+        public string $sprite,
+        public string $group,
+        public int $x,
+        public int $y,
+        public int $w,
+        public int $h,
+    ) {
+    }
+}
---
+++ src/Serializer/Normalizer/ProfileIconNormalizer.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Zeggriim\RiotApiDataDragon\Serializer\Normalizer;
+
+use Symfony\Component\Serializer\Normalizer\DenormalizerAwareInterface;
+use Symfony\Component\Serializer\Normalizer\DenormalizerAwareTrait;
+use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
+use Zeggriim\RiotApiDataDragon\Dto\ProfileIcon\ProfileIcon;
+use Zeggriim\RiotApiDataDragon\Dto\ProfileIcon\ProfileIconImage;
+
+final class ProfileIconNormalizer implements DenormalizerInterface, DenormalizerAwareInterface
+{
+    use DenormalizerAwareTrait;
+
+    public function denormalize(mixed $data, string $type, ?string $format = null, array $context = []): mixed
+    {
+        return new ProfileIcon(
+            $data['id'],
+            isset($data['image']) ? $this->denormalizer->denormalize($data['image'], ProfileIconImage::class, $format, $context) : null,
+        );
+    }
+
+    public function supportsDenormalization(mixed $data, string $type, ?string $format = null, array $context = []): bool
+    {
+        return ProfileIcon::class === $type;
+    }
+
+    public function getSupportedTypes(?string $format): array
+    {
+        return [ProfileIcon::class => true];
+    }
+}
