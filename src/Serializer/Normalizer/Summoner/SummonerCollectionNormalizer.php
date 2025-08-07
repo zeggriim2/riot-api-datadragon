d12b56ad457a42535d0e7b44d80ac9a0fda60b34
zeggriim <zeggriim@gmail.com>
2025-08-07 20:14:14 +0200

    feat(Dto) : ZEG-45 Add DTO Summoner return api Summoner

---
+++ src/Serializer/Normalizer/Summoner/SummonerCollectionNormalizer.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Zeggriim\RiotApiDataDragon\Serializer\Normalizer\Summoner;
+
+use Symfony\Component\Serializer\Normalizer\DenormalizerAwareInterface;
+use Symfony\Component\Serializer\Normalizer\DenormalizerAwareTrait;
+use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
+use Zeggriim\RiotApiDataDragon\Dto\Summoner\Summoner;
+use Zeggriim\RiotApiDataDragon\Dto\Summoner\SummonerCollection;
+
+final class SummonerCollectionNormalizer implements DenormalizerInterface, DenormalizerAwareInterface
+{
+    use DenormalizerAwareTrait;
+
+    public function denormalize(mixed $data, string $type, ?string $format = null, array $context = []): SummonerCollection
+    {
+        $summoners = [];
+        foreach ($data['data'] as $summonerName => $summoner) {
+            $summoners[$summonerName] =  $this->denormalizer->denormalize($summoner, Summoner::class, $format, $context);
+        }
+
+        return new SummonerCollection(
+            $data['type'],
+            $data['version'],
+            $summoners,
+        );
+    }
+
+    public function supportsDenormalization(mixed $data, string $type, ?string $format = null, array $context = []): bool
+    {
+        return SummonerCollection::class === $type;
+    }
+
+    public function getSupportedTypes(?string $format): array
+    {
+        return [SummonerCollection::class => true];
+    }
+}
